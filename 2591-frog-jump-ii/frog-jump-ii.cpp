eec3fc2dd5853ca800e8d1684881152f0981babc
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2025-11-13 00:47:59 +0530

    Time: 4 ms (21.60%) | Memory: 69.1 MB (79.84%) - LeetSync

---
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+        int ans = 0;
+        if(stones.size() == 2){
+            return stones[1]-stones[0];
+        }
+        for(int i =2;i < stones.size();i++){
+            ans = max(ans,stones[i]-stones[i-2]);
+        }
+        return ans;
+    }
+};
