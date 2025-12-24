5e0e5b6216ea2af0ea656702fdd3b18e4471152a
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2025-12-24 18:29:17 +0530

    Time: 3 ms (27.74%) | Memory: 10.4 MB (5.43%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,24 +1,59 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long k = x;
+        // long long k = x;
         if(x == 0){
             return x;
         }
+        string help = "";
+        if(x==INT_MIN){
+            help += to_string(x%(-10));
+            x /= 10;
+        }
         bool neg = false;
-        if(k < 0){
+        if(x < 0){
             neg = true;
-            k = -k;
+            x = -x;
         }
-        string temp = to_string(k);
-       std::reverse(temp.begin(), temp.end());
-        long long conv = stoll(temp);
-        if(neg){
-            conv = -conv;
+        string temp = to_string(x);
+        if(help.size() != 0){
+           temp += help;
         }
-        if(conv >= INT_MAX || conv <= INT_MIN){
-            return 0;
+
+        std::reverse(temp.begin(), temp.end());
+
+        int maxp = INT_MAX;
+        string maxi = to_string(maxp);
+        int last = INT_MIN%(10);
+
+        int minp = INT_MIN/(-10);
+        string mini = to_string(minp);
+        mini += to_string(abs(last));
+        
+        if(temp.size() < maxi.size()){
+            return neg == false ? stoi(temp) : -(stoi(temp));
         }
-        return (int)conv;
+        if(neg == false){
+          for(int i =0;i<temp.size();i++){
+            if(temp[i]-'0' > maxi[i]-'0'){
+                return 0;
+            }
+            else if(temp[i]-'0' < maxi[i]-'0'){
+                    break;
+                }
+        }
+        }
+        else{
+            for(int i = 0;i<temp.size();i++){
+                if(temp[i]-'0' > mini[i]-'0'){
+                    return 0;
+                }
+                else if(temp[i]-'0' < mini[i]-'0'){
+                    break;
+                }
+            }
+        }
+        int final = stoi(temp);
+         return neg == false ? final : -(final);
     }
 };
