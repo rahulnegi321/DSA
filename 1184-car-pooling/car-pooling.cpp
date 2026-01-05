b5d2874f7aed8ca6d0af0b12e3189155a8216b66
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-01-05 23:29:29 +0530

    Time: 2 ms (41.94%) | Memory: 14.2 MB (89.64%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -6,18 +6,19 @@ public:
         sort(trips.begin(),trips.end(),[](const vector<int>&a,vector<int>&b){
             return a[1] < b[1];
         });
-        for(int i = 0;i<trips.size();i++){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
 
+        for(int i = 0;i<trips.size();i++){
             int start = trips[i][1];
-            for(int j = 0;j<=start;j++){
-                curr -= mpp[j];
-                mpp[j] = 0;
+            while(!q.empty() && q.top().first <= start){
+                curr -= q.top().second;
+                q.pop();
             }
             curr += trips[i][0];
             if(curr > capacity){
                 return false;
             }
-            mpp[trips[i][2]] = mpp[trips[i][2]] + trips[i][0];
+            q.push({trips[i][2],trips[i][0]});
         }
         return true;
     }
