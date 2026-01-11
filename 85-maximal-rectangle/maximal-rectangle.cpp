46d8e2a437920d1efd7d6d6eb55f5adf83e784a9
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-01-12 00:40:54 +0530

    Time: 16 ms (12.78%) | Memory: 23.6 MB (5.12%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -3,46 +3,55 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-         int answer = 0;
         vector<int>prev(m,0);
+        int ans = 0;
         for(int i = 0;i<n;i++){
-            for(int  j = 0;j<m;j++){
-               if(matrix[i][j] == '0'){
-                prev[j] = 0;
-               }
-               else{
-                prev[j]++;
+            vector<int>curr(m,0);
+            for(int j = 0;j<m;j++){
+               if(matrix[i][j] == '1'){
+                 curr[j] = 1+prev[j];
                }
             }
-
-        stack<pair<int,int>>st;
-        stack<pair<int,int>>st2;
-        vector<int>before(m,-1);
-        for(int k = 0;k<m;k++){
-            while(!st.empty() && st.top().first >= prev[k]){
+            prev = curr;
+            stack<pair<int,int>>st;
+            vector<int>left;
+            int k = 0;
+            while(k < m){
+                while(!st.empty() && st.top().first >= curr[k]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    left.push_back(-1);
+                }
+                else{
+                    left.push_back(st.top().second);
+                }
+                st.push({curr[k],k});
+                k++;
+            }
+            k = m-1;
+            while(st.size()){
                 st.pop();
             }
-            if(st.size() != 0){
-                before[k] = st.top().second;
+            vector<int>right;
+            while(k >= 0){
+                while(!st.empty() && st.top().first >= curr[k]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    right.push_back(m);
+                }
+                else{
+                    right.push_back(st.top().second);
+                }
+                st.push({curr[k],k});
+                k--;
             }
-            st.push({prev[k],k});
-        }
-    
-        vector<int>after(m,m);
-         for(int k = m-1;k>=0;k--){
-            while(!st2.empty() && st2.top().first >= prev[k]){
-                st2.pop();
+            reverse(right.begin(),right.end());
+            for(int j = 0;j<m;j++){
+                ans = max(ans,(right[j]-1-(left[j]+1)+1)*curr[j]); 
             }
-            if(st2.size() != 0){
-                after[k] = st2.top().second;
-            }
-            st2.push({prev[k],k});
-         }
-            for(int k = 0;k<m;k++){
-                int temp = prev[k]*(after[k] - before[k] - 1);
-                answer = max(answer,temp);
         }
-        }
-        return answer;
+        return ans;
     }
 };
