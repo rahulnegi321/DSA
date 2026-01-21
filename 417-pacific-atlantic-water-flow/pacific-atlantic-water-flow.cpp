ccca1acd3b07ced467ae28bda465f4a3ab91d73f
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-01-22 01:17:20 +0530

    Time: 7 ms (62.16%) | Memory: 24.3 MB (30.92%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,81 +1,68 @@
 class Solution {
 public:
-    bool dfs(int row,int col,vector<vector<int>>&heights,vector<vector<int>>&vis,vector<vector<bool>>&ans){
-        int n = heights.size();
-        int m = heights[0].size();
-        if(row == heights.size()-1 || col == heights[0].size()-1){
-            return true;
-        }
-        vis[row][col] = 1;
-        // if(ans[row][col] == true){
-        //     return true;
-        // }
-        int drow[] = {1,0,-1,0};
-        int dcol[] = {0,-1,0,1};
-        bool temp = false;
-        for(int i = 0;i<4;i++){
-            int nrow = drow[i] + row;
-            int ncol = dcol[i] + col;
-
-            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] == 0 && heights[nrow][ncol] <= heights[row][col]){
-              temp = temp || dfs(nrow,ncol,heights,vis,ans);
-            }
-        }
-        vis[row][col] = 0;
-        return temp;
-    }
-
-    bool dfs1(int row,int col,vector<vector<int>>&heights,vector<vector<int>>&vis,vector<vector<bool>>&ans){
-        if(row == 0 || col == 0){
-            return true;
-        }
-         int n = heights.size();
-        int m = heights[0].size();
-        vis[row][col] = 1;
-        // if(ans[row][col] == true){
-        //     return true;
-        // }
-        int drow[] = {1,0,-1,0};
-        int dcol[] = {0,-1,0,1};
-        bool temp = false;
-        for(int i = 0;i<4;i++){
-            int nrow = drow[i] + row;
-            int ncol = dcol[i] + col;
-
-            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] == 0 && heights[nrow][ncol] <= heights[row][col]){
-              temp = temp || dfs1(nrow,ncol,heights,vis,ans);
-            }
-        }
-        vis[row][col] = 0;
-        return temp;
-    }
-
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        vector<vector<bool>>ans(n,vector<bool>(m,false));
+        queue<pair<int,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                    bool temp = true;
-                    temp = temp && dfs(i,j,heights,vis,ans);
-                    if(temp == false){
-                        continue;
-                    }
-                    temp = temp && dfs1(i,j,heights,vis,ans);
-                    if(temp == true){
-                        ans[i][j] = true;
-                    }
-                }
+            q.push({i,0});
+            vis[i][0] = 1;
         }
-        vector<vector<int>>finalans;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(ans[i][j] == true){
-                    finalans.push_back({i,j});
+        for(int j = 0;j<m;j++){
+            q.push({0,j});
+            vis[0][j] = 1;
+        }
+        while(q.size()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            int drow[] = {1,0,-1,0};
+            int dcol[] = {0,-1,0,1};
+            for(int i = 0;i<4;i++){
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] == 0 && heights[nrow][ncol] >= heights[row][col]){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
                 }
             }
         }
-        return finalans;
+        
+        queue<pair<int,int>>p;
+        vector<vector<int>>vis1(n,vector<int>(m,0));
+        for(int i = 0;i<n;i++){
+            p.push({i,m-1});
+            vis1[i][m-1] = 1;
+        }
+        for(int j = 0;j<m;j++){
+            p.push({n-1,j});
+            vis1[n-1][j] = 1;
+        }
+    
+         while(p.size()){
+            int row = p.front().first;
+            int col = p.front().second;
+            p.pop();
+            int drow[] = {1,0,-1,0};
+            int dcol[] = {0,-1,0,1};
+            for(int i = 0;i<4;i++){
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis1[nrow][ncol] == 0 && heights[nrow][ncol] >= heights[row][col]){
+                    vis1[nrow][ncol] = 1;
+                    p.push({nrow,ncol});
+                }
+            }
+        }
+        vector<vector<int>>ans;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(vis[i][j] == 1 && vis1[i][j] == 1){
+                    ans.push_back({i,j});
+                }
+            }
+        }
+        return ans;
     }
 };
