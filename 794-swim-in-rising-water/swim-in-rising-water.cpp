29bfd1ee70a9ce1fd59c14b23414ffa87276fa27
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-01-22 01:36:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (87.56%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-      int n = grid.size();
-      vector<vector<int>>dis(n,vector<int>(n,INT_MAX));
-      priority_queue<pair<int,pair<int,int>>>q;
-      q.push({-grid[0][0],{0,0}});
-      int ans = INT_MAX;
-      while(!q.empty()){
-        int distance = -q.top().first;
-        int row = q.top().second.first;
-        int col = q.top().second.second;
-        q.pop();
-        if(row == n-1 && col == n-1){
-          ans = min(ans,distance);
-          continue;
-        }
-        int drow[] = {1,0,0,-1};
-        int dcol[] = {0,-1,1,0};
-        for(int i =0;i<4;i++){
-            int nrow = drow[i] + row;
-            int ncol = dcol[i] + col;
-            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < n && dis[nrow][ncol] > max(distance,grid[nrow][ncol])){
-                dis[nrow][ncol] = max(distance,grid[nrow][ncol]);
-                q.push({-dis[nrow][ncol],{nrow,ncol}});
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dis(n,vector<int>(m,3000));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        q.push({grid[0][0],{0,0}});
+        while(q.size()){
+            int curr = q.top().first;
+            int row = q.top().second.first;
+            int col = q.top().second.second;
+            q.pop();
+            if(curr > dis[row][col]){
+                continue;
+            }
+            if(row == n-1 && col == m-1){
+                return curr;
+            }
+            int drow[] = {0,-1,0,1};
+            int dcol[] = {-1,0,1,0};
+            for(int i = 0;i<4;i++){
+                int nrow = drow[i] + row;
+                int ncol = dcol[i] + col;
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && dis[nrow][ncol] > max(curr,grid[nrow][ncol])){
+                    dis[nrow][ncol] = max(curr,grid[nrow][ncol]);
+                    q.push({max(curr,grid[nrow][ncol]),{nrow,ncol}});
+                }
             }
         }
-      }
-      return ans;   
+        return 1;
     }
 };
