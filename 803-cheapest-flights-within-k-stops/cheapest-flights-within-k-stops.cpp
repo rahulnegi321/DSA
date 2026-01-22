73ccf7328b83e34bda0a6cfee8df63c863f28a8e
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-01-22 20:25:10 +0530

    Time: 9 ms (19.66%) | Memory: 19.2 MB (21.01%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,77 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        int m = flights[0].size();
         vector<vector<pair<int,int>>>adj(n);
-        vector<bool>vis(n,false);
-        int maxflights = k+1;
-        vector<vector<int>>dis(n,vector<int>(maxflights+1,INT_MAX));
-        dis[src][0] =0;
-       for(auto it:flights){
-        adj[it[0]].push_back({it[1],it[2]});
-       }
-       priority_queue<pair<int,pair<int,int>>>q;
-       q.push({0,{src,0}});
-       while(!q.empty()){
-        int distance = -q.top().first;
-        int node = q.top().second.first;
-        int count = q.top().second.second;
-        q.pop();
-        if(node == dst){
-            return distance;
+        for(auto it : flights){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        if(count == maxflights){
-            continue;
-        }
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        q.push({0,{0,src}});
        
-        for(auto it : adj[node]){
-          int b = it.first;
-          int w = it.second;
-          if(distance + w < dis[b][count+1]){
-            dis[b][count+1] = distance+w;
-            q.push({-dis[b][count+1],{b,count+1}});
-          }
+        map<int,map<int,int>>vis;
+         vis[src][0] = 0;
+        int ans = -1;
+        while(q.size()){
+            int curr = q.top().first;
+            int count = q.top().second.first;
+            int node = q.top().second.second;
+            q.pop();
+            if(curr > vis[node][count]){
+                continue;
+            }
+            if(count > k+1){
+                continue;
+            }
+            if(node == dst && count <= k+1){
+               return curr;
+            }
+            for(auto it : adj[node]){
+                int temp = it.first;
+                int dis = it.second;
+                if(temp != src){
+                    if(!vis.count(temp)){
+                        vis[temp][count+1] = curr+dis;
+                        q.push({curr+dis,{count+1,temp}});
+                    }
+                    else{
+                       if(vis[temp].count(count+1)){
+                            if(curr+dis < vis[temp][count+1]){
+                                vis[temp][count+1] = curr+dis;
+                                bool need = true;
+                                for(auto it : vis[temp]){
+                                    if(it.first == count+1){
+                                        break;
+                                    }
+                                    if(it.second <= curr+dis){
+                                        need = false;
+                                        break;
+                                    }
+                                }
+                                if(!need){
+                                    continue;
+                                }
+                                q.push({curr+dis,{count+1,temp}});
+                            }
+                        }
+                        vis[temp][count+1] = curr+dis;
+                        bool need = true;
+                                for(auto it : vis[temp]){
+                                    if(it.first == count+1){
+                                        break;
+                                    }
+                                    if(it.second <= curr+dis){
+                                        need = false;
+                                        break;
+                                    }
+                                }
+                                if(!need){
+                                    continue;
+                                }
+                        q.push({vis[temp][count+1],{count+1,temp}});
+                    }
+                }
+            }
         }
-       }
-       return -1;
+        return ans;
     }
 };
