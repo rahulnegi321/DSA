c7c1dfd13c7cb7cfc7fcf4c300fd9f7438b4ea64
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-01-27 01:32:02 +0530

    Time: 36 ms (25.25%) | Memory: 27 MB (62.67%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int helper(string& s,string& t,int ind,int idx,vector<vector<int>>&dp){
-        if(idx == t.size()){
+    int helper(int ind,int idx,string& s,string& t,vector<vector<int>>&dp){
+       if(ind >= s.size()){
+         if(idx >= t.size()){
             return 1;
-        }
-        if(ind == s.size()){
-            return 0;
-        }
-        if(dp[ind][idx] != -1){
-            return dp[ind][idx];
-        }
-        int nottake = 0;
-        int take = 0;
-        if(s[ind] == t[idx]){
-            take = helper(s,t,ind+1,idx+1,dp);
-        }
-        nottake = helper(s,t,ind+1,idx,dp);
-        return dp[ind][idx] = (take+nottake);
+         }
+         return 0;
+       }
+       if(dp[ind][idx] != -1){
+        return dp[ind][idx];
+       }
+       int take = 0;
+       int nottake = 0;
+       if(idx < t.size() && s[ind] == t[idx]){
+           take = helper(ind+1,idx+1,s,t,dp);
+       }
+       nottake = helper(ind+1,idx,s,t,dp);
+       return dp[ind][idx] = take + nottake;
     }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        if(s == t){
-            return 1;
-        }
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(s,t,0,0,dp);
+        vector<vector<int>>dp(n,vector<int>(m+1,-1));
+        return helper(0,0,s,t,dp);
     }
 };
