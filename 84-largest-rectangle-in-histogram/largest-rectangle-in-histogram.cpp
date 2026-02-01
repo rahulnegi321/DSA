29a62302d60f29bdb131957ad7520e93e2231999
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-02-01 22:09:24 +0530

    Time: 30 ms (30.67%) | Memory: 85.7 MB (41.75%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n= heights.size();
-        stack<pair<int,int>>st;
-        stack<pair<int,int>>st2;
-        vector<int>before(n,-1);
-        for(int i = 0;i<heights.size();i++){
-            while(!st.empty() && st.top().first >= heights[i]){
+        stack<int>st;
+        int n = heights.size();
+        vector<int>left(n);
+        vector<int>right(n);
+        for(int i = 0;i<n;i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
                 st.pop();
             }
-            if(st.size() != 0){
-                before[i] = st.top().second;
-            }
-            st.push({heights[i],i});
+            left[i] = st.empty() ? 0 : st.top()+1;
+            st.push(i);
         }
-    
-        vector<int>after(n,n);
-         for(int i = n-1;i>=0;i--){
-            while(!st2.empty() && st2.top().first >= heights[i]){
-                st2.pop();
-            }
-            if(st2.size() != 0){
-                after[i] = st2.top().second;
-            }
-            st2.push({heights[i],i});
-         }
-            int answer = 0;
-            for(int i = 0;i<n;i++){
-                int temp = heights[i]*(after[i] - before[i] - 1);
-                answer = max(answer,temp);
+        while(st.size()){
+            st.pop();
         }
-        return answer;
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            right[i] = st.empty() ? n-1 : st.top()-1;
+            st.push(i);
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            ans = max(ans,heights[i]*(right[i]-left[i]+1));
+        }
+        return ans;
     }
 };
