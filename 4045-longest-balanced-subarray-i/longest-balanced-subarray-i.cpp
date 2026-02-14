e0a7e8e2a476e0b566b16b3f2da4cd3d2a505640
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-02-14 18:14:27 +0530

    Time: 1876 ms (61.35%) | Memory: 530.5 MB (9.79%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        int ans = 0;
+        int count = 0;
         for(int i = 0;i<nums.size();i++){
-            unordered_map<int,int>mpp1;
-            unordered_map<int,int>mpp2;
+            unordered_map<int,int>even;
+            unordered_map<int,int>odd;
+
             for(int j = i;j<nums.size();j++){
-                if(nums[j]%2 == 0){
-                    mpp2[nums[j]]++;
-                }
-                else{
-                mpp1[nums[j]]++;
-                }
-                if(mpp1.size() == mpp2.size()){
-                    ans = max(ans,j-i+1);
-                }
+               if(nums[j]%2 == 0){
+                even[nums[j]]++;
+               }
+               else{
+                odd[nums[j]]++;
+               }
+
+               if(even.size() == odd.size()){
+                count = max(count,j-i+1);
+               }
             }
         }
-        return ans;
+        return count;
     }
 };
