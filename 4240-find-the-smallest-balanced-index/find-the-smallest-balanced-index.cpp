59e1ff17a8b6c8c5e8c5818608fcb64c8834ba93
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-13 21:28:57 +0530

    Time: 6 ms (81.50%) | Memory: 192.4 MB (89.70%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.cpp
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.cpp
@@ -8,7 +8,11 @@ public:
         for(int i = n-1;i>=0;i--){
             sum -= nums[i];
             if(i < n-1){
-                product = product*nums[i+1];
+             long long temp = ceil((double)sum/product);
+             if(temp < nums[i+1]){
+                break;
+             }
+             product = product*1LL*nums[i+1];
             }
             if(product > sum){
                 break;
