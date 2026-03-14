a7cfd5bb422a7095ce2a9daae6ec28c2eb5fabf4
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-15 01:37:04 +0530

    Time: 31 ms (28.75%) | Memory: 24.3 MB (16.61%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,28 +3,30 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
         vector<vector<int>>vis(n,vector<int>(m,0));
+        int count = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(vis[i][j] == 1 || grid[i][j] == '0'){
+                if(grid[i][j] == '0' || vis[i][j] == 1){
                     continue;
                 }
                 count++;
                 queue<pair<int,int>>q;
-                vis[i][j] = 1;
                 q.push({i,j});
                 while(q.size()){
                     int row = q.front().first;
                     int col = q.front().second;
                     q.pop();
-                    int drow[] = {-1,0,1,0};
-                    int dcol[] = {0,1,0,-1};
+                    if(vis[row][col]){
+                        continue;
+                    }
+                    vis[row][col] = 1;
+                    int drow[] = {0,1,0,-1};
+                    int dcol[] = {1,0,-1,0};
                     for(int k = 0;k<4;k++){
                         int nrow = drow[k] + row;
                         int ncol = dcol[k] + col;
-                        if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
-                            vis[nrow][ncol] = 1;
+                        if(nrow < n && ncol < m && nrow >= 0 && ncol >= 0 && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0){
                             q.push({nrow,ncol});
                         }
                     }
