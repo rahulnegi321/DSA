b2c73bdbea07dfa1093811b8a83970596bd314f6
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-14 13:39:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.34%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -3,37 +3,23 @@ public:
     string ans = "";
     int count = 0;
     void helper(string &curr,int n,int k){
-
-        if(count > k){
-            return;
-        }
+        if(count > k){return;}
         if(curr.size() == n){
            count++;
            if(count == k){
             ans = curr;
-            return;
-           }
-        }
+            return;}}
         if(curr.size() < n){
              for(char i = 'a';i<='c';i++){
             if(curr.size() == 0){
                 curr += i;
                 helper(curr,n,k);
-                curr.pop_back();
-            }
-            else{
-                char temp = curr.back();
-                if(temp == i){
-                    continue;
-                }
+                curr.pop_back();}
+            else{char temp = curr.back();if(temp == i){continue;}
                 curr += i;
                 helper(curr,n,k);
-                curr.pop_back();
-            }
-        }
-        }
-        return;
-    }
+                curr.pop_back();}}}
+        return;}
     string getHappyString(int n, int k) {
        string temp = "";
        helper(temp,n,k);
