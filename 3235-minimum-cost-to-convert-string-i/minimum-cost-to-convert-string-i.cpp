0650776515ed9c8c2f945e72f81b464049a30144
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-17 21:56:39 +0530

    Time: 474 ms (5.04%) | Memory: 127.5 MB (7.46%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        map<pair<char,char>,int>dis;
+        map<pair<char,char>,int>mpp;
         priority_queue<pair<int,pair<char,char>>,vector<pair<int,pair<char,char>>>,greater<pair<int,pair<char,char>>>>q;
-        unordered_map<char,vector<pair<char,int>>>mpp;
         for(int i = 0;i<original.size();i++){
             q.push({cost[i],{original[i],changed[i]}});
-            mpp[original[i]].push_back({changed[i],cost[i]});
+        }
+        unordered_map<char,vector<pair<char,int>>>adj;
+        for(int i = 0;i<original.size();i++){
+            adj[original[i]].push_back({changed[i],cost[i]});
         }
         while(q.size()){
-            int dist = q.top().first;
-            char curr = q.top().second.first;
-            char to = q.top().second.second;
+            int paise = q.top().first;
+            int so = q.top().second.first;
+            int ta = q.top().second.second;
             q.pop();
-            if(dis.count({curr,to})){
+            if(mpp.count({so,ta})){
                 continue;
             }
-            dis[{curr,to}] = dist;
-            for(auto it : mpp[to]){
+            mpp[{so,ta}] = paise;
+            for(auto it : adj[ta]){
                 char temp = it.first;
-                int charge = it.second;
-                if(!dis.count({curr,temp})){
-                    q.push({dist+charge,{curr,temp}});
+                int pa = it.second;
+                if(mpp.count({so,temp})){
+                    continue;
                 }
+                if(temp == so){
+                    continue;
+                }
+                q.push({paise+pa,{so,temp}});
             }
         }
-        long long count = 0;
-        for(int i = 0;i<target.size();i++){
+        long long ans = 0;
+        for(int i = 0;i<source.size();i++){
             if(source[i] == target[i]){
                 continue;
             }
-            if(!dis.count({source[i],target[i]})){
+            if(!mpp.count({source[i],target[i]})){
                 return -1;
             }
-            count += dis[{source[i],target[i]}];
+            ans += mpp[{source[i],target[i]}];
         }
-        return count;
+        return ans;
     }
 };
