bf3f44dbcc58d741df8610e3be05c03cbe76b418
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-21 18:02:10 +0530

    Time: 1866 ms (15.57%) | Memory: 204.1 MB (10.78%) - LeetSync

---
+++ 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int ispalindrome(int left,int right,string &s,vector<vector<int>>&dp){
+        if(dp[left][right] != -1){
+            return dp[left][right];
+        }
+        int l = left;
+        int r = right;
+        while(left <= right){
+            if(s[left++] != s[right--]){
+                return dp[l][r] = 0;
+            }
+        }
+        return dp[l][r] = 1;
+    }
+    int helper(int ind,int count,string &s,vector<vector<int>>&dp,vector<vector<int>>&dp2){
+        if(count == 3 && ind == s.size()){
+            return 1;
+        }
+        if(ind == s.size()){
+            return 0;
+        }
+        if(count > 3){
+            return 0;
+        }
+        if(dp[ind][count] != -1){
+            return dp[ind][count];
+        }
+        int ans = 0;
+        for(int i = ind;i<s.size();i++){
+            if(ispalindrome(ind,i,s,dp2)){
+                ans = ans | helper(i+1,count+1,s,dp,dp2);
+            }
+        }
+        return dp[ind][count] =  ans;
+    }
+    bool checkPartitioning(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        vector<vector<int>>dp2(n,vector<int>(n,-1));
+        return helper(0,0,s,dp,dp2);
+    }
+};
