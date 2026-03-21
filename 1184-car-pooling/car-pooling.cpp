614bf856770edaf28de8b72c7cc02d915782ebfc
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-21 15:18:57 +0530

    Time: 5 ms (15.44%) | Memory: 14 MB (95.71%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        unordered_map<int,int>mpp;
-        int curr = 0;
-        sort(trips.begin(),trips.end(),[](const vector<int>&a,vector<int>&b){
-            return a[1] < b[1];
+        sort(trips.begin(),trips.end(),[](vector<int>&a,vector<int>&b){
+            if(a[1] != b[1]){
+              return a[1] < b[1];
+            }
+            if(a[2] != b[2]){
+              return a[2] < b[2];
+            }
+            return a[0] < b[0];
         });
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-
+        int curr = 0;
         for(int i = 0;i<trips.size();i++){
-            int start = trips[i][1];
-            while(!q.empty() && q.top().first <= start){
-                curr -= q.top().second;
-                q.pop();
+            while(q.size() && q.top().first <= trips[i][1]){
+              curr -= q.top().second;
+              q.pop();
             }
             curr += trips[i][0];
             if(curr > capacity){
