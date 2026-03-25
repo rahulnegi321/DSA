36006741b0a17fa80cffbaab2efb3e8b20c14863
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-26 00:59:14 +0530

    Time: 47 ms (10.81%) | Memory: 17.4 MB (89.63%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>t(n,1);
-        vector<int>count(n,1);
-        for(int i = 1;i<n;i++){
-            for(int j = 0;j<i;j++){
-                if(nums[i] > nums[j]){
-                   if(t[i] == t[j]+1){
-                     count[i] = count[i] + count[j];
-                   }
-                   else if(t[i] < t[j] + 1){
-                     t[i] = t[j]+1;
-                     count[i] = count[j];
-                   }
-                }
-            }
-        }
-        int maxi = *max_element(t.begin(),t.end());
-        int ans = 0;
+        vector<int>dp(n,1);
+        vector<int>dp2(n,1);
+        int maxi = 1;
         for(int i = 0;i<n;i++){
-            if(t[i] == maxi){
-                ans = ans + count[i];
+            int count = 0;
+            for(int j = 0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    int prev = dp[i]; 
+                    dp[i] = max(dp[i],dp[j]+1);
+                    maxi = max(dp[i],maxi);
+                    if(dp[i] == dp[j]+1){
+                        if(prev == dp[i]){
+                            count += dp2[j];
+                        }
+                        else{
+                            count = dp2[j];
+                        }
+                    }
+                }
+                dp2[i] = max(1,count);
             }
         }
-        return ans;
-     }
+        if(maxi == 1){
+            return nums.size();
+        }
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(dp[i] == maxi){
+                count += dp2[i];
+            }
+        }
+        return count;
+    }
 };
