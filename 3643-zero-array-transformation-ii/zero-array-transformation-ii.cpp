8312fdece919abbcbbae70aff159736acd99b9ff
Rahul Negi <148367568+rahulnegi321@users.noreply.github.com>
2026-03-27 02:44:45 +0530

    Time: 119 ms (8.84%) | Memory: 394 MB (5.16%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
-    bool check(vector<int>&nums,vector<vector<int>>&queries,int mid){
+    bool helper(int total,vector<int>&nums,vector<vector<int>>&queries){
         int n = nums.size();
         vector<int>temp(n+1,0);
-        for(int i = 0;i<=mid;i++){
+        for(int i = 0;i<total;i++){
             temp[queries[i][0]] -= queries[i][2];
             temp[queries[i][1]+1] += queries[i][2];
         }
         vector<int>pre;
         int sum = 0;
         for(int i = 0;i<n;i++){
-            sum = sum + temp[i];
+            sum += temp[i];
             pre.push_back(sum);
         }
-        for(int i =0;i<nums.size();i++){
-            if(nums[i] + pre[i] > 0){
+        for(int i = 0;i<n;i++){
+            if(pre[i] + nums[i] > 0){
                 return false;
             }
         }
         return true;
     }
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
+        int n = queries.size();
         int low = 0;
-        int high = queries.size()-1;
+        int high = n;
         int ans = -1;
-        bool zero = true;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] != 0){
-                zero = false;
-                break;
-            }
-        }
-        if(zero){
-            return 0;
-        }
         while(low <= high){
-          int mid = low + (high-low)/2;
-
-          if(check(nums,queries,mid)){
-            ans = mid+1;
-            high = mid-1;
-          }
-          else{
-            low = mid+1;
-          }
+            int mid = low + (high-low)/2;
+            if(helper(mid,nums,queries)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
         return ans;
     }
